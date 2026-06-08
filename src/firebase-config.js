4acebc278ee3d179f1d4cad9d8dc03fe5694fd5d
Krishna Sharma <priyasharma6348@gmail.com>
2026-06-08 11:00:27 +0530

    Move Firebase config to env

--- src/firebase-config.js
+++ src/firebase-config.js
@@ -5,13 +5,13 @@ import { getAuth } from "firebase/auth";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCKE7EzS8mEQxOhTiSofHG0u8NLKHsMXjo",
-  authDomain: "crud-app-73bcf.firebaseapp.com",
-  projectId: "crud-app-73bcf",
-  storageBucket: "crud-app-73bcf.firebasestorage.app",
-  messagingSenderId: "130155703349",
-  appId: "1:130155703349:web:8a60165c8f6ab499a7557c",
-  measurementId: "G-M6P1JEKQQM",
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
 };
 
 const app = initializeApp(firebaseConfig);
